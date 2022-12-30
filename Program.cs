9eafc5a0e88c54552f3f19eac2ea9ce80f508817
ShehabAlsharkawy <121675258+ShehabAlsharkawy@users.noreply.github.com>
2022-12-30 21:06:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ConsoleApp11
+﻿namespace ConsoleApp13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstnumber = 0, secondnumber = 0;
+            bool isPrime = true;
+
+            Console.WriteLine("Enter the lower bound first number:");
+            firstnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper bound second number:");
+            secondnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The prime numbers between {0} and {1} are:", firstnumber, secondnumber);
+
+            for (int i = firstnumber; i <= secondnumber; i++)
+            {
+                isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
+        
+    
